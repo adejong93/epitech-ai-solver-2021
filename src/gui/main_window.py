68e0d2a908f9f230b5aa08d271b430be142cffae
Louis Grante <louis.grante@epitech.eu>
2022-01-06 15:47:18 +0100

    fix main window class

--- src/gui/main_window.py
+++ src/gui/main_window.py
@@ -2,7 +2,7 @@ from PyQt5 import QtWidgets, uic
 from .ressource.logo import *
 
 
-class MainWindow(QtWidgets.QWidget):
+class MainWindow(QtWidgets.QMainWindow):
     def __init__(self) -> None:
         super(MainWindow, self).__init__()
         uic.loadUi('./src/gui/template/main_window.ui', self)
